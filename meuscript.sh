aaaa6fcd22e3dfba17f5f622e5a276aec4767ce8
filisteus <filisteus@gmail.com>
2022-11-11 20:23:52 -0300

    Sobre o echo

---
+++ meuscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Inicio"
+:<<'TERMINAL' #adicionado :<< e a palavra ele irá ignorar tudo no intervalo delas 
+echo "é isso"
+echo "vamos"
+echo "mais uma vez"
+echo "de novo"
+TERMINAL
+echo "Olá, mundo!"
